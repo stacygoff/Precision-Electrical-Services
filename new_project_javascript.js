e4a16f220ac6c6051d648d4866c75f60188afd5d
stacygoff <stacygoff001@gmail.com>
2018-10-10 21:16:25 -0400

    changed java script

--- new_project_javascript.js
+++ new_project_javascript.js
@@ -1,4 +1,26 @@
-var visitor = prompt('How can I help you?')
-var message = 'welcome + to Precision Electrical Services';
+function myFunction() {
+    var text;
+    var whatService = prompt("Which service do you need?");
+    switch (whatService) {
+        case "Residential":
+            text = "Someone will be in touch soon";
+            break;
+        case "Commercial":
+            text = "Someone will be in touch soon";
+            break;
+    }
+    document.getElementById("demo").innerHTML = text;
+};
 
-document.write(message);
+
+// var visitor = prompt('How can I help you?')
+// var message = 'welcome + to Precision Electrical Services';
+
+// document.write(message);
+
+
+
+// var visitor = prompt('How can I help you?')
+// var message = 'welcome + to Precision Electrical Services';
+
+// document.write(message);
